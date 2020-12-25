f9374ea9aec35361342ed4cdfd6c7be7a5d18f93
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-25 10:58:36 +0700

    Set action bar title

--- app/src/main/java/io/erikrios/github/myviewandviews/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myviewandviews/MainActivity.kt
@@ -1,11 +1,13 @@
 package io.erikrios.github.myviewandviews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Google Pixel"
     }
 }
